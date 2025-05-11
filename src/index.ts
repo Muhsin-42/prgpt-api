1fe2f46fdb8a2aec0984280c9d9307001a0c2010
Muhsin-42 <muhsinofficial21@gmail.com>
2025-05-11 14:14:57 +0530

    feat: added rate limitting for api

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import {Hono} from "hono";
 import {logger} from "hono/logger";
+import {getConnInfo} from "hono/bun";
 import {poweredBy} from "hono/powered-by";
 import {env} from "./config/env.config";
 import {cors} from "hono/cors";
 import {prRoutes} from "./routes/pr.routes";
+import {rateLimiter} from "hono-rate-limiter";
 const app = new Hono();
 
 // Global middlewares
@@ -11,14 +13,27 @@ app.use("*", logger());
 app.use("*", poweredBy());
 app.use("*", cors());
 
+const limiter = rateLimiter({
+  windowMs: 60 * 60 * 1000,
+  limit: 25,
+  standardHeaders: "draft-7",
+  keyGenerator: (c) => getConnInfo(c)?.remote?.address!,
+});
+
+app.use("*", (c, next) => {
+  console.log(getConnInfo(c)?.remote?.address);
+  return next();
+});
+app.use("*", limiter);
+
 // Health Check
-app.get("/", (c) => c.text("✅ API is live"));
-app.get("/health", (c) => c.text("🏥 Ping is live and kicking!"));
+app.get("/", (c) => c.text("✅ PrGPT API is live"));
+app.get("/health", (c) => c.text("🏥 PrGPT API is live and kicking!"));
 app.route("api/pr/generate-title-description", prRoutes);
 // Catch-all 404
 app.notFound((c) =>
   c.json(
-    {message: "Aapke dwaara dial kiya gaya route asthithwa me nahi he"},
+    {message: "The subscriber you have dialed is not in service 💀☠️"},
     404
   )
 );
