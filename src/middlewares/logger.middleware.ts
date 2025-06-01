2571174a16b819a9e85b1ad0989ce8143cf57448
Muhsin-42 <muhsinofficial21@gmail.com>
2025-06-01 13:02:43 +0530

    fix: removed logging for get request

--- src/middlewares/logger.middleware.ts
+++ src/middlewares/logger.middleware.ts
@@ -1,68 +1,11 @@
-// import {getConnInfo} from "hono/bun";
-// import {logRequest} from "../services/db/logging.service";
-// import {Context, Next} from "hono";
-
-// export const trackRequests = async (c: Context, next: Next) => {
-//   const requestStartTime = Date.now();
-//   const requestId = crypto.randomUUID();
-//   const forwardedFor = c.req.header("x-forwarded-for");
-//   const realIp =
-//     forwardedFor?.split(",")[0]?.trim() ||
-//     getConnInfo(c)?.remote?.address ||
-//     "unknown";
-//   const ipAddress = realIp;
-//   const method = c.req.method;
-//   const url = c.req.url;
-//   const {currentUrl} = await c.req.json();
-
-//   let requestBody = null;
-//   try {
-//     if (["POST", "PUT", "PATCH"].includes(method)) {
-//       requestBody = await c.req.json().catch(() => null);
-//     }
-//   } catch (error) {
-//     console.error("Error reading request body:", error);
-//   }
-
-//   await next();
-
-//   const requestDuration = Date.now() - requestStartTime;
-//   const responseStatus = c.res.status;
-
-//   let responseBody = null;
-
-//   try {
-//     const clonedRes = c.res.clone();
-//     responseBody = await clonedRes.json().catch(() => null);
-//   } catch (error) {
-//     console.error("Error reading response body:", error);
-//   }
-
-//   const logEntry = {
-//     requestId,
-//     timestamp: new Date(),
-//     ipAddress,
-//     method,
-//     url,
-//     currentUrl,
-//     requestBody,
-//     responseStatus,
-//     responseBody,
-//     duration: requestDuration,
-//   };
-
-//   logRequest(logEntry).catch((err) => {
-//     console.error("Logging error:", err);
-//   });
-
-//   return c.res;
-// };
-
 import {getConnInfo} from "hono/bun";
 import {logRequest} from "../services/db/logging.service";
 import {Context, Next} from "hono";
 
 export const trackRequests = async (c: Context, next: Next) => {
+  if (c.req.method === "GET") {
+    return next();
+  }
   const requestStartTime = Date.now();
   const requestId = crypto.randomUUID();
 
